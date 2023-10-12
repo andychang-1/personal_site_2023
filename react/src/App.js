6cc709833b8cac3cda7dae2e50e5a995bfc458f9
Andy <yuanchanga@gmail.com>
2023-10-12 19:50:31 -0400

    ad

--- react/src/App.js
+++ react/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter,
+  Link,
+  Outlet,
+  Route,
+  Router,
+  Routes,
+  useParams,
+} from "react-router-dom";
 
-function App() {
+const BlogPosts = {
+  "first-blog-post": {
+    title: "First Blog Post",
+    description: "Lorem ipsum dolor sit amet, consectetur adip.",
+  },
+  "second-blog-post": {
+    title: "Second Blog Post",
+    description: "Hello React Router v6",
+  },
+};
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>404: Page Not Found</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return <div className="App"></div>;
+}
