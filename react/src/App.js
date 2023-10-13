ab71eaaa127f793116982973ba15622602fde2aa
Andy <yuanchanga@gmail.com>
2023-10-12 22:29:22 -0400

    Wouter test

--- react/src/App.js
+++ react/src/App.js
@@ -1,52 +1,18 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter,
-  Link,
-  Outlet,
-  Route,
-  Router,
-  Routes,
-  useParams,
-} from "react-router-dom";
 
-const BlogPosts = {
-  "first-blog-post": {
-    title: "First Blog Post",
-    description: "Lorem ipsum dolor sit amet, consectetur adip.",
-  },
-  "second-blog-post": {
-    title: "Second Blog Post",
-    description: "Hello React Router v6",
-  },
-};
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>About View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>404: Page Not Found</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
-}
+import { Link, Route } from "wouter";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Link href="/users/1">
+        <a className="link">Profile2</a>
+      </Link>
+      <Route path="/about">About Us</Route>
+      <Route path="/users/:name">
+        {(params) => <div>Hello, {params.name}!</div>}
+      </Route>
+    </div>
+  );
 }
